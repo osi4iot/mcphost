433bdece703bbe4108cc48de587bb905f8f62068
Rui Chen <rui@chenrui.dev>
2025-09-02 06:53:37 -0400

    fix `--version` output (#128)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/charmbracelet/fang"
@@ -11,6 +12,13 @@ import (
 var version = "dev"
 
 func main() {
+	for _, arg := range os.Args[1:] {
+		if arg == "--version" || arg == "-v" {
+			fmt.Printf("mcphost version %s\n", version)
+			os.Exit(0)
+		}
+	}
+
 	rootCmd := cmd.GetRootCommand(version)
 	if err := fang.Execute(context.Background(), rootCmd); err != nil {
 		os.Exit(1)
